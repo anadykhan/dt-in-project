bfd0378d69ce3be81926c2424ae84a619018cc06
anadykhan <aiyoun.khan@gmail.com>
2024-05-07 12:34:50 +0600

    Home banner section done

--- src/components/home/banner/Banner.jsx
+++ src/components/home/banner/Banner.jsx
@@ -1,57 +1,85 @@
-import { Typography } from '@mui/material'
-import { Box } from '@mui/system'
-import CommonButton from '../../general/button/CommonButton'
+import React from 'react';
+import { Typography } from '@mui/material';
+import { Box } from '@mui/system';
+import CommonButton from '../../general/button/CommonButton';
 
 const Banner = () => {
     return (
         <Box
             sx={{
                 width: 'full',
-                height: 650,
-                display: 'flex',
-                flexDirection: 'column',
-                justifyContent: 'center',
-                alignItems: 'center',
+                height: 800,
+                position: 'relative',
                 backgroundImage: `url(src/assets/seth-doyle-zf9_yiAekJs-unsplash.jpg)`,
                 backgroundSize: 'cover',
                 backgroundPosition: 'center',
-                color: 'white'
+                display: 'flex',
+                justifyContent: 'center'
             }}
         >
+            {/* Pseudo-element for dimming effect */}
             <Box
                 sx={{
+                    position: 'absolute',
+                    top: 0,
+                    left: 0,
+                    width: '100%',
+                    height: '100%',
+                    backgroundColor: 'rgba(0, 0, 0, 0.5)', // Adjust alpha for desired dimness
+                    zIndex: 1, // Ensure it's above the background image
+                }}
+            />
+            <Box
+                sx={{
+                    height: '100%',
+                    position: 'relative',
+                    zIndex: 2,
                     display: 'flex',
                     flexDirection: 'column',
                     justifyContent: 'center',
-                    alignItems: 'center'
+                    alignItems: 'center',
+                    color: 'white',
+                    width: 700,
+                    gap: 3
                 }}
             >
-                <Typography>
+                <Box
+                    sx={{
+                        height: 3,
+                        width: 100,
+                        bgcolor: 'red'
+                    }}
+                >
+
+                </Box>
+                <Typography gutterBottom>
                     FUNDRAISING AND CHARITY FOR POOR
                 </Typography>
                 <Box>
-                    <Typography>
+                    <Typography variant="h2" textAlign='center' fontWeight='bold' gutterBottom>
                         Inspire and Help Them
                     </Typography>
-                    <Typography>
+                    <Typography variant="h2" textAlign='center' fontWeight='bold' gutterBottom>
                         Live Longer
                     </Typography>
                 </Box>
-                <Typography
-                    sx={{
-                        textAlign: 'center'
-                    }}
-                >
+                <Typography variant="body1" gutterBottom sx={{ textAlign: 'center' }}>
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem officiis nesciunt at dolore voluptates, deserunt sed pariatur architecto ducimus consequatur?
                 </Typography>
+                <Box
+                sx={{
+                    marginTop: 5
+                }}
+                >
+                    <CommonButton
+                        content='LEARN MORE'
+                        background='red'
+                        width={200}
+                    />
+                </Box>
             </Box>
-            <CommonButton
-                content='LEARN MORE'
-                background='red'
-                width={230}
-            ></CommonButton>
         </Box>
-    )
-}
+    );
+};
 
-export default Banner
+export default Banner;
