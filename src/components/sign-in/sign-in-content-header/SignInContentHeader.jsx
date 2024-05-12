be42fd099e1a02df73272ab30e3648c8e1314600
anadykhan <aiyoun.khan@gmail.com>
2024-05-12 16:27:52 +0600

    Sign in header done

---
+++ src/components/sign-in/sign-in-content-header/SignInContentHeader.jsx
@@ -0,0 +1,26 @@
+import { Box, Typography } from '@mui/material'
+import React from 'react'
+
+const SignInContentHeader = () => {
+  return (
+      <Box
+      sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          gap: 3
+      }}
+      >
+          <Typography>
+              GET INVOLVED NOW
+          </Typography>
+          <Typography variant='h4' fontWeight='bold'>
+              Our goal is to help the poor people
+          </Typography>
+          <Typography>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa accusantium nulla nihil tempore dicta! Ratione quibusdam.
+          </Typography>
+      </Box>
+  )
+}
+
+export default SignInContentHeader
--- src/components/sign-in/sign-in-content/SignInContent.jsx
+++ src/components/sign-in/sign-in-content/SignInContent.jsx
@@ -1,11 +1,12 @@
 import { Box } from '@mui/material'
 import React from 'react'
 import SignInContentBox from '../sign-in-content-box/SignInContentBox'
+import SignInContentHeader from '../sign-in-content-header/SignInContentHeader'
 
 const SignInContent = () => {
   return (
     <Box>
-        Hello
+        <SignInContentHeader></SignInContentHeader>
         <SignInContentBox></SignInContentBox>
     </Box>
   )
