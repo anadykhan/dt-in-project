8040623a9da863f9473115ad05d3090d5aa80821
anadykhan <aiyoun.khan@gmail.com>
2024-05-15 11:59:38 +0600

    Events router setup done

--- server/src/routes/api.js
+++ server/src/routes/api.js
@@ -3,11 +3,13 @@ const express = require('express')
 const donationsRouter = require('./donations/donations.router')
 const usersRouter = require('./users/users.router')
 const causesRouter = require('./causes/causes.router')
+const eventsRouter = require('./events/events.router')
 
 const api = express.Router()
 
 api.use('/donations', donationsRouter)
 api.use('/users', usersRouter)
 api.use('/causes', causesRouter)
+api.use('/events', eventsRouter)
 
 module.exports = api
