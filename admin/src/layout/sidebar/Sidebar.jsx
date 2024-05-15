51d9b05cfa11a289fd9b61202ed11918110e4887
anadykhan <aiyoun.khan@gmail.com>
2024-05-15 18:14:15 +0600

    Admin panel navbar done

---
+++ admin/src/layout/sidebar/Sidebar.jsx
@@ -0,0 +1,42 @@
+import { Box, Button } from '@mui/material';
+import React from 'react';
+import { NavLink } from "react-router-dom"
+
+const navList = ['Causes', 'Events', 'Users', 'Logout']
+
+const Sidebar = () => {
+
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        background: '#001528',
+        width: '15%',
+        height: '100vh',
+        padding: 3,
+        gap: 5,
+      }}
+    >
+      {
+        navList.map((data, index) => {
+          return (
+            <NavLink
+            key={index}
+            >
+              <Button
+              sx={{
+                color: 'white',
+                fontSize: 18
+              }}
+              >
+                {data}
+              </Button>
+            </NavLink>
+          )
+        })
+      }
+    </Box>
+  );
+};
+export default Sidebar;
---
+++ admin/src/pages/root/Root.jsx
@@ -0,0 +1,16 @@
+import { Box } from '@mui/material'
+import React from 'react'
+import Sidebar from '../../layout/sidebar/Sidebar'
+
+const Root = () => {
+    return (
+        <Box
+        sx={{
+        }}
+        >
+            <Sidebar></Sidebar>
+        </Box>
+    )
+}
+
+export default Root
