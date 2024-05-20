558574cbdbe73c5c62fdb8e17a4f8e87acc79f92
anadykhan <aiyoun.khan@gmail.com>
2024-05-20 14:57:36 +0600

    Users and Events load more button done

--- admin/src/pages/events-table-page-admin/EventsTablePageAdmin.jsx
+++ admin/src/pages/events-table-page-admin/EventsTablePageAdmin.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react';
 import Box from '@mui/material/Box';
 import EventsTableBarAdmin from '../../components/events-table-admin/events-table-bar-admin/EventsTableBarAdmin';
 import EventsTableContentAdmin from '../../components/events-table-admin/events-table-content-admin/EventsTableContentAdmin';
@@ -9,17 +9,25 @@ import TableCell from '@mui/material/TableCell';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import TableBody from '@mui/material/TableBody';
+import Button from '@mui/material/Button';
 import { useGetEventsData } from '../../hooks/hooks/event-hooks/EventHooks';
 
 const EventsTablePageAdmin = () => {
+  const initialDataCount = 15;
+  const additionalDataCount = 10;
 
-  const { data: eventsData, isLoading: eventsDataLoading } = useGetEventsData()
+  const { data: eventsData, isLoading: eventsDataLoading } = useGetEventsData();
+  const [displayedDataCount, setDisplayedDataCount] = useState(initialDataCount);
+
+  const handleLoadMore = () => {
+    setDisplayedDataCount(prevCount => prevCount + additionalDataCount);
+  };
 
   if (eventsDataLoading) {
-    return <h1>Loading</h1>
+    return <h1>Loading</h1>;
   }
 
-  // console.log(eventsData)
+  const remainingDataCount = eventsData.data.length - displayedDataCount;
 
   return (
     <Box
@@ -30,7 +38,7 @@ const EventsTablePageAdmin = () => {
         gap: 4
       }}
     >
-      <EventsTableBarAdmin></EventsTableBarAdmin>
+      <EventsTableBarAdmin />
       <TableContainer component={Paper}>
         <Table sx={{ minWidth: 650 }} aria-label="simple table">
           <TableHead>
@@ -43,23 +51,28 @@ const EventsTablePageAdmin = () => {
           </TableHead>
           <TableBody>
             {
-              eventsData.data.map((data, index) => {
-                return (
-                  <EventsTableContentAdmin
-                    key={index}
-                    title={data.title}
-                    dateTime={data.dateTime}
-                    location={data.location}
-                    data={data}
-                  ></EventsTableContentAdmin>
-                )
-              })
+              eventsData.data.slice(0, displayedDataCount).map((data, index) => (
+                <EventsTableContentAdmin
+                  key={index}
+                  title={data.title}
+                  dateTime={data.dateTime}
+                  location={data.location}
+                  data={data}
+                />
+              ))
             }
           </TableBody>
         </Table>
       </TableContainer>
+      {remainingDataCount > 0 && (
+        <Box mt={2} textAlign="center">
+          <Button variant="contained" color="primary" onClick={handleLoadMore}>
+            Load More
+          </Button>
+        </Box>
+      )}
     </Box>
-  )
-}
+  );
+};
 
-export default EventsTablePageAdmin
+export default EventsTablePageAdmin;
--- admin/src/pages/users-table-page-admin/UsersTablePageAdmin.jsx
+++ admin/src/pages/users-table-page-admin/UsersTablePageAdmin.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react';
 import Box from '@mui/material/Box';
 import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
@@ -7,19 +7,27 @@ import TableCell from '@mui/material/TableCell';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import TableBody from '@mui/material/TableBody';
+import Button from '@mui/material/Button';
 import { useGetUsersData } from '../../hooks/hooks/users-hook/UsersHook';
 import UsersTableBarAdmin from '../../components/users-table-admin/users-table-bar-admin/UsersTableBarAdmin';
 import UsersTableContentAdmin from '../../components/users-table-admin/users-table-content-admin/UsersTableContentAdmin';
 
 const UsersTablePageAdmin = () => {
+  const initialDataCount = 15;
+  const additionalDataCount = 10;
 
-  const { data: usersData, isLoading: usersDataLoading } = useGetUsersData()
+  const { data: usersData, isLoading: usersDataLoading } = useGetUsersData();
+  const [displayedDataCount, setDisplayedDataCount] = useState(initialDataCount);
+
+  const handleLoadMore = () => {
+    setDisplayedDataCount(prevCount => prevCount + additionalDataCount);
+  };
 
   if (usersDataLoading) {
-    return <h1>Loading</h1>
+    return <h1>Loading</h1>;
   }
 
-//   console.log(usersData.data)
+  const remainingDataCount = usersData.data.length - displayedDataCount;
 
   return (
     <Box
@@ -30,7 +38,7 @@ const UsersTablePageAdmin = () => {
         gap: 4
       }}
     >
-      <UsersTableBarAdmin></UsersTableBarAdmin>
+      <UsersTableBarAdmin />
       <TableContainer component={Paper}>
         <Table sx={{ minWidth: 650 }} aria-label="simple table">
           <TableHead>
@@ -43,24 +51,28 @@ const UsersTablePageAdmin = () => {
           </TableHead>
           <TableBody>
             {
-              usersData.data.map((data, index) => {
-                //   console.log(data.username)
-                return (
-                  <UsersTableContentAdmin
-                    key={index}
-                    username={data.username}
-                    password={data.password}
-                    email={data.email}
-                    data={data}
-                  ></UsersTableContentAdmin>
-                )
-              })
+              usersData.data.slice(0, displayedDataCount).map((data, index) => (
+                <UsersTableContentAdmin
+                  key={index}
+                  username={data.username}
+                  password={data.password}
+                  email={data.email}
+                  data={data}
+                />
+              ))
             }
           </TableBody>
         </Table>
       </TableContainer>
+      {remainingDataCount > 0 && (
+        <Box mt={2} textAlign="center">
+          <Button variant="contained" color="primary" onClick={handleLoadMore}>
+            Load More
+          </Button>
+        </Box>
+      )}
     </Box>
-  )
-}
+  );
+};
 
-export default UsersTablePageAdmin
+export default UsersTablePageAdmin;
