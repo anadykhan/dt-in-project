c7f532f4553d7c76ce14a6b6d9bcf09de9ce8e7c
anadykhan <aiyoun.khan@gmail.com>
2024-05-20 13:00:47 +0600

    Users table content admin done

--- admin/src/components/users-table-admin/users-table-content-admin/UsersTableContentAdmin.jsx
+++ admin/src/components/users-table-admin/users-table-content-admin/UsersTableContentAdmin.jsx
@@ -1,11 +1,45 @@
-import React from 'react'
+import TableCell from '@mui/material/TableCell';
+import TableRow from '@mui/material/TableRow';
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+import { NavLink } from 'react-router-dom';
+import { useDeleteUserData } from '../../../hooks/hooks/users-hook/UsersHook';
 
-const UsersTableContentAdmin = () => {
+export default function UsersTableContentAdmin(props) {
+
+  const { mutate: deleteUser } = useDeleteUserData()
+
+  const { username, password, email, data } = props
+
+  const handleUsersDelete = () => {
+    deleteUser(data)
+    console.log('Delete working!')
+  }
+
   return (
-    <div>
-      
-    </div>
-  )
+    <TableRow
+      sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+    >
+      <TableCell
+        component="th"
+        scope="row"
+        sx={{
+          fontWeight: 'bold'
+        }}>
+        {username}
+      </TableCell>
+      <TableCell align="right">{password}</TableCell>
+      <TableCell align="right">{email}</TableCell>
+      <TableCell align="right">
+        <Box>
+          <NavLink to={`/events-edit/${data._id}`}>
+            <Button>Edit</Button>
+          </NavLink>
+          <Button
+            onClick={handleUsersDelete}
+          >Delete</Button>
+        </Box>
+      </TableCell>
+    </TableRow>
+  );
 }
-
-export default UsersTableContentAdmin
