529630d61802b3978da9e02ba859c769fc94822d
Aiyoun Khan <aiyounkhan@Aiyouns-Mac-mini.local>
2024-05-22 00:48:41 +0600

    Admin done

--- server/src/models/admins/admins.model.js
+++ server/src/models/admins/admins.model.js
@@ -1,18 +1,45 @@
-const mongoose = require('mongoose')
+const admins = require('./admins.mongo')
 
-const adminsSchema = new mongoose.Schema({
-    username: {
-        type: String,
-        required: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
-    email: {
-        type: String,
-        required: true
-    }
-})
+async function getAllAdmins() {
+    return await admins
+        .find({})
+        .sort({ createdAt: -1 })
+}
 
-module.exports = mongoose.model('Admin', adminsSchema)
+async function getSingleAdmin(adminID) {
+    return await admins
+        .findById(adminID)
+}
+
+async function createAdmin(admin) {
+    const newAdmin = new admins(admin)
+    await newAdmin.save()
+}
+
+async function updateAdmin(admin) {
+    await admins
+        .findByIdAndUpdate(
+            admin._id,
+            {
+                $set: admin,
+            },
+            {
+                new: true,
+            }
+        )
+}
+
+async function deleteAdmin(admin) {
+    await admins
+        .findByIdAndDelete(
+            admin._id
+        )
+}
+
+module.exports = {
+    getAllAdmins,
+    getSingleAdmin,
+    createAdmin,
+    updateAdmin,
+    deleteAdmin
+}
